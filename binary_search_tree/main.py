c98ebb33a514030bb7c8c1873b808eb76f2cb8a5
gesifeta <adamgemechu@gmail.com>
2025-06-17 17:40:26 +0300

    added contains  method

--- binary_search_tree/main.py
+++ binary_search_tree/main.py
@@ -29,7 +29,23 @@ class BinarySearchTree:
                     return True
                 temp = temp.right 
        
-      
+    def contains(self,value):
+        temp = self.root
+        while(True):
+           
+            if temp.value == value:
+                return True
+            if value > temp.value:
+                if temp.right is None:
+                    return False
+                temp = temp.right
+            else:
+                if temp.left is None:
+                    return False
+                temp = temp.left
+            
+
+
 
 my_tree = BinarySearchTree()
 
@@ -41,5 +57,5 @@ my_tree.insert(26)
 print(my_tree.root.value)
 print(my_tree.root.left.value)
 print(my_tree.root.right.value)
-print(my_tree.root.right.left.value)
- 
+print(my_tree.root.left.right.value)
+print(my_tree.contains(7))
